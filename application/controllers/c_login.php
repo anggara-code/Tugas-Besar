5ffa382c58b4a511f2c826a805f65aeb0612f170
DESKTOP-LV65BV4\anggara <anggaramahendra00@gmail.com>
2018-06-27 21:29:13 +0700

    Mengganti autoload, routes dan Menambahkan controllers c_login

---
+++ application/controllers/c_login.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class C_login extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('v_login');		
+	}
+
+}
+
+/* End of file c_login.php */
+/* Location: ./application/controllers/c_login.php */
